5f805e71a7e702f9909ba091a5314925e94cacdb
Ibm-Arab <68500608+Ibm-Arab@users.noreply.github.com>
2021-05-21 12:35:14 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    password: {
+        type:  String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user',User);

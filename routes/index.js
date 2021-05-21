ab09ae314a255eb3534d79b8bde40929d899e472
Ibm-Arab <68500608+Ibm-Arab@users.noreply.github.com>
2021-05-21 13:02:52 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ const User = require('../model/user');
 
 module.exports = Router.use((req,res,next) => {
         if(req.isAuthenticated()) {
-            res.status(200).json({message: "Welcome you have logged In successfully",
-                                  logInSession: req.session});
+            User.findById(req.session.passport.user)
+            .then(result => {
+                res.status(200).json({message: "Welcome you have logged In successfully",
+                                  logInSession: req.session,
+                                loggedInAs: result});
+            })
+            .catch(err => {
+                console.log(err);
+            })
         } else {
             res.status(403).json({message : "Please Log In"});
         }
